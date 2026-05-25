1eede0d402e99f2eaa6117a43f342730fbe8d6bf
FouleesRanvillaises <fouleesranvillaises@gmail.com>
2026-05-25 19:43:15 +0200

    Update site URL and base path in config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://fouleesranvillaises.github.io/',
-    base: '/website'
+    site: 'https://www.lesfouleesranvillaises.fr/',
+    base: '/'
 });
